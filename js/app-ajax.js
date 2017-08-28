ce0b2b6ae5effec8639dd18ad7d90d4201f66815
AkkaFranKebne <ewawrobel@gmail.com>
2017-08-28 09:40:20 +0200

    sending lat, lng, date  to json via php

--- js/app-ajax.js
+++ js/app-ajax.js
@@ -21,18 +21,27 @@ $(function() {
       imagesDiv.empty();
       //add updated content
     $.each(images, function(index, element) {
-        var div = $('<div>', {class: "gallery-box"});
+        var galleryBox = $('<div>', {class: "gallery-box"});
         var h3 = $('<h3>', {class: "title"}).text(element.title);
-        var p = $('<p>', {class: "desc"}).text(element.alt);
-        div.append(h3).append(p);
-        imagesDiv.append(div);
-        var div2 = $('<div>', {class: "image-box"});
+        var p1 = $('<p>', {class: "time"}).text(element.date);
+        var p2 = $('<p>', {class: "desc"}).text(element.alt);
+        galleryBox.append(h3).append(p1).append(p2);
+        imagesDiv.append(galleryBox);
+        var imageBox = $('<div>', {class: "image-box"});
         var picture = $('<picture>', {class: "gallery"});
         var img = $('<img>', {class: "gallery-image"}).attr('src', element.photo);
-        div.append(div2);
-        div2.append(picture);
+        galleryBox.append(imageBox);
+        imageBox.append(picture);
         picture.append(img);
-        
+        var addressBox = $('<div>', {class: "address-box"});
+        var p3 = $('<p>', {class: "coord"}).text("lat: ");
+        var p4 = $('<p>', {class: "coord"}).text(element.lat);
+        var p5 = $('<p>', {class: "coord"}).text("lng: ");
+        var p6 = $('<p>', {class: "coord"}).text(element.lng);
+        var p7 = $('<p>', {class: "address"});
+        var map = $('<div>', {class: "map"}).attr('id', element.id);
+        addressBox.append(p3).append(p4).append(p5).append(p6).append(p7).append(map);
+        galleryBox.append(addressBox);
     });
   }
     
